11bcbd0ab9c9ff09ae64047db0ba6dbef19a18ff
Kyle Coffey <me@phytolizer.dev>
2022-04-04 08:08:29 -0500

    impl step0

--- impls/cpp-phyto/step0_repl.cpp
+++ impls/cpp-phyto/step0_repl.cpp
@@ -1,1 +1,14 @@
-int main() {}
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string line_buffer;
+    while (true) {
+        std::cout << "mal> " << std::flush;
+        if (!std::getline(std::cin, line_buffer)) {
+            break;
+        }
+
+        std::cout << line_buffer << '\n';
+    }
+}
